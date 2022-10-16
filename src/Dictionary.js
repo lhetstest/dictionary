10bd87c17e7601ec2fb467c95b67e740563b6f70
12345 <a12345@MacBook-Pro-12345.local>
2022-10-16 22:05:31 +0200

    feat: the api and axios added

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,19 @@
 import React, {useState} from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 const Dictionary = () => {
     let [keyword, setKeyword] = useState("")
+
+    const handleResponse = (response) => {
+        console.log(response.data[0])
+    }
     const search = (e) => {
         e.preventDefault();
-        alert(`Searching for ${keyword} definition...`);
+
+        // docs find on Dictionary API https://dictionaryapi.dev/
+        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+        axios.get(apiUrl).then(handleResponse);
     }
 
     const handleKeywordChange = (e) => {
