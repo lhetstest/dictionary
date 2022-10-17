73071318938868308698f29ffbdd7aef5d4e9906
12345 <a12345@MacBook-Pro-12345.local>
2022-10-17 18:24:12 +0200

    feat: the synonyms added

---
+++ src/Synonyms.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Synonyms = (props) => {
+    if (props.synonyms) {
+        return (
+            <ul>
+                {props.synonyms.map((synonym, index)=> {
+                    return <li key={index}>{synonym}</li>
+                })}
+            </ul>
+        )
+    } else {
+        return null;
+    }
+}
+
+export default Synonyms;
